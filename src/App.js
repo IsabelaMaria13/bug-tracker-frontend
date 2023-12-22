5829490530e8f352778ed28b34c9e160c8a14113
VladVorniceanu <vorniceanu.v@gmail.com>
2023-12-22 10:41:57 +0200

    [main]Pagina register - interfata + conexiune cu click pe buton pagina Auth + fixes interfata auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Authentication from "./Authentication";
+import Register from "./Register";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
        <Route path="/" element={<Authentication />} /> 
        <Route path="/home" element={<Home/>}/>
+       <Route path="/register" element = {<Register />} />
       </Routes>
     </Router>
   );
